89a14763367f8397952247851373a0c7fc8e8e36
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2026-02-20 11:45:54 -0300

    Código Oi mundo

---
+++ oi_mundo.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // Declarando as variáveis
+    int valor;
+    int valor2;
+    int soma;
+
+    printf("Oi mundo\n");
+    printf("Oi Fatec\n");
+    printf("Oi Fatec Rio Claro\n\n");
+    valor = 10;
+    valor2 = 20;
+    soma = valor + valor2;
+    printf("A soma é %i\n\n", soma);
+    valor2 = "Oi mundo";
+}
---
+++ oi_mundo.py
@@ -0,0 +1,8 @@
+print('oi mundo')
+print('oi Fatec')
+print('Oi Fatec Rio Claro')
+
+valor = 10
+valor2 = 20
+soma = valor + valor2
+print('A soma é: ', soma)
